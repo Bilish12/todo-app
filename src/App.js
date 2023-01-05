1be3ad57343dc1c41544ab30bfbfde9fd58ff0c9
Bilish12 <bil.kharbuja100@gmail.com>
2023-01-05 23:38:27 +0545

    hello

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="myContainer">
+      
+      <p id="title">my first todo app.
+      </p>
+    </div>
+    
+  )
+}
+
+export default App;
